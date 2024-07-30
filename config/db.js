b774809e6205ce85dc6332749ed3df5dbb80a800
l3miage-benaisil <benaissailian@outlook.fr>
2024-07-30 10:04:19 +0200

    fix flash

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-require('dotenv').config({ path: '.env.local', override: true });
+require('dotenv').config({ path: '.env', override: true });
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Root@123",
-  database: "yanishlali_student"
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
+console.log(process.env.DB_HOST);
 connection.connect((err) => {
   if (err) {
     console.error('Erreur de connexion : ' + err.stack);
